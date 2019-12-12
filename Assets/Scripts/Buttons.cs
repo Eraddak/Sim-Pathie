00d72df1407a36c2e490e06e7d93ad2c184238e2
François cresson <francois.cresson@isen.yncrea.fr>
2019-12-12 12:42:14 +0100

    First interface push
        - Menu
        - Interface visuelle

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+
+    public bool isVROn;
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+}
