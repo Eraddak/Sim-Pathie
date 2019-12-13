c3338695f503b9732f4217393daa0bd83c185157
François cresson <francois.cresson@isen.yncrea.fr>
2019-12-13 13:46:57 +0100

    First VR push from ui

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
 
-    public bool isVROn;
-
-    public void LoadScene(int index)
+    public void LoadVisualScene()
     {
-        SceneManager.LoadScene(index);
+        Toggle toggle = GetComponentInChildren<Toggle>();
+        if (toggle.isOn) SceneManager.LoadScene("Visual VR");
+        else SceneManager.LoadScene("Visual");
+    }
+
+    public void LoadUIScene()
+    {
+        SceneManager.LoadScene("UI");
+    }
+
+    public void LoadAuditoryScene()
+    {
+        SceneManager.LoadScene("Auditory");
     }
 
     public void QuitApp()
